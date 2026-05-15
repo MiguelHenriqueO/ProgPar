802fd3a633c658de54cb3e596220e5d74d6d756d
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2026-05-14 21:36:43 -0300

    a

---
+++ bd/src/main/java/dc/unifacef/bd/model/Produto.java
@@ -0,0 +1,18 @@
+package dc.unifacef.bd.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String name;
+    @Column
+    private Double price;
+    @Column (length = 500)
+    private String desc;
+}
